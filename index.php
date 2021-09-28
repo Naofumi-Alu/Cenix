ff59720df63814dfb6c5dac6380f6cc1d8cd147c
Naofumi-Alu <joseabrahammaciastorres@gmail.com>
2021-09-27 20:36:42 -0500

    Version Movile 1.3.7 carrusel and card en progress- casi hecho,falta  organizar texto

--- index.php
+++ index.php
@@ -102,24 +102,12 @@
 				<!--formato de 4 columnas en blanco ala izquierda-->
 				<div class="col-md-4">	
 				</div>
-				<div class="card">
-
+				
 				<!-- Card del carrusel -->
-				<div class="card" style="width: 18rem;">
-					<img src="..." class="card-img-top" alt="...">
+				<div class="card col-md-4" >
 
-					
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
-					</div>
-				</div>
-
-				</div>
 					<!--Carrusel con controles-->
-					<div id="carouselExampleCaptions" class="col-md-4 carousel slide" data-bs-ride="carousel">
-
+					<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
 
 						<div class="carousel-indicators">
 							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -131,85 +119,93 @@
 							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
 						</div>
 
-					
-
 						<div class="carousel-inner">
-								<div class="carousel-item active">
-									<img src="./src_php/public/images/Soluciones/tics370x250.jpg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>First slide label</h5>
-										<p>Some representative placeholder content for the first slide.</p>
+									<div class="carousel-item active">
+										<img src="./src_php/public/images/Soluciones/tics370x250.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>First slide label</h5>
+											<p>Some representative placeholder content for the first slide.</p>
+										</div>
 									</div>
-								</div>
-								<div class="carousel-item">
-									<img src="./src_php/public/images/Soluciones/Edificiosinteligentes370x250.jpeg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>Second slide label</h5>
-										<p>Some representative placeholder content for the second slide.</p>
+									
+									<div class="carousel-item">
+										<img src="./src_php/public/images/Soluciones/Edificiosinteligentes370x250.jpeg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Second slide label</h5>
+											<p>Some representative placeholder content for the second slide.</p>
+										</div>
 									</div>
-								</div>
-								
-								<div class="carousel-item">
-									<img src="./src_php/public/images/Soluciones/Movilidad370x250.jpg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>Third slide label</h5>
-										<p>Some representative placeholder content for the third slide.</p>
+									
+									<div class="carousel-item">
+										<img src="./src_php/public/images/Soluciones/Movilidad370x250.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Third slide label</h5>
+											<p>Some representative placeholder content for the third slide.</p>
+										</div>
 									</div>
+
+									<div class="carousel-item">
+										<img src="./src_php/public/images/Soluciones/Seguridadvial370x250.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Third slide label</h5>
+											<p>Some representative placeholder content for the third slide.</p>
+										</div>
+									</div>
+
+									<div class="carousel-item">
+										<img src="./src_php/public/images/Soluciones/software370x250.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Third slide label</h5>
+											<p>Some representative placeholder content for the third slide.</p>
+										</div>
+									</div>
+
+									<div class="carousel-item">
+										<img src="./src_php/public/images/Soluciones/IoT370x250.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Third slide label</h5>
+											<p>Some representative placeholder content for the third slide.</p>
+										</div>
+									</div>
+
+									<div class="carousel-item">
+										<img src="./src_php/public/images/Soluciones/Energia370x250.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Third slide label</h5>
+											<p>Some representative placeholder content for the third slide.</p>
+										</div>
+									</div>
+
 								</div>
 
-								<div class="carousel-item">
-									<img src="./src_php/public/images/Soluciones/Seguridadvial370x250.jpg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>Third slide label</h5>
-										<p>Some representative placeholder content for the third slide.</p>
-									</div>
-								</div>
 
-								<div class="carousel-item">
-									<img src="./src_php/public/images/Soluciones/software370x250.jpg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>Third slide label</h5>
-										<p>Some representative placeholder content for the third slide.</p>
-									</div>
-								</div>
 
-								<div class="carousel-item">
-									<img src="./src_php/public/images/Soluciones/IoT370x250.jpg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>Third slide label</h5>
-										<p>Some representative placeholder content for the third slide.</p>
-									</div>
-								</div>
+								<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="visually-hidden">Previous</span>
 
-								<div class="carousel-item">
-									<img src="./src_php/public/images/Soluciones/Energia370x250.jpg" class="d-block w-100" alt="...">
-									<div class="carousel-caption d-none d-md-block">
-										<h5>Third slide label</h5>
-										<p>Some representative placeholder content for the third slide.</p>
-									</div>
-								</div>
+							</button>
+
+							<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="visually-hidden">Next</span>
+							</button>
 
 							</div>
-						
-						
-						
-							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-							<span class="visually-hidden">Previous</span>
-						
-						</button>
 
-						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-							<span class="visually-hidden">Next</span>
-						</button>
+
 					
+						<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
 					</div>
-
+ 				</div>
+				</div>
 			</div>
 		</div>	
 
-			<!-- our work portfolio -->
+		<!-- our work portfolio -->
 				<div class="our-work brown-bg">
 				
 						<div class="container">
